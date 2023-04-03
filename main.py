9bf0f46ac927fcee07c0bad4e53a04a42d1ad30e
beasttamer01 <118001345+beasttamer01@users.noreply.github.com>
2023-04-03 16:59:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -246,8 +246,8 @@ def echo(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
         app.send_message(message.chat.id, '__You are either not **Authorized** or **Banned**__', reply_to_message_id=message.id,reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Source Code", url="https://github.com/bipinkrish/Mdisk-Downloader-Bot")]]))
         return
     
-    app.send_message(message.chat.id, f'__Hi {message.from_user.mention}, I am Mdisk Video Downloader, you can watch Downloaded Videos without MX Player.\n\nSend me a link to Start... or click /help to check usage__',reply_to_message_id=message.id,
-    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Source Code", url="https://github.com/bipinkrish/Mdisk-Downloader-Bot")]]))
+    app.send_message(message.chat.id, f'__Hi {message.from_user.mention}, \n\nIam Mdisk Link Downloader\n\nThe Videos That Are Downloaded Here Can Be Played In Any Media Player Without MX Player\n\nClick /help For More Information On Using The Bot___',reply_to_message_id=message.id,
+    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🛡 Telugu Dubbed Movies 🛡", url="https://t.me/TeluguDubbedMovies_HollyWood")]]))
 
 
 # help command
